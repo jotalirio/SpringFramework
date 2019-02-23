a8bd636f9dba565a143a5503f66f28b0cbd0dd57
USERSAD\jliriovi <jliriovi@everis.com>
2019-02-23 16:59:55 +0000

    Util 'Constants' file created.

---
+++ Java/spring-boot-data-jpa/src/main/java/com/example/springboot/app/utils/Constants.java
@@ -0,0 +1,11 @@
+package com.example.springboot.app.utils;
+
+public interface Constants {
+
+  // Query words
+  public static final String FROM = "FROM";
+  
+  /***** Tables JPA *****/
+  // Table 'Clients'
+  public static final String CLIENTS = "Clients";
+}
