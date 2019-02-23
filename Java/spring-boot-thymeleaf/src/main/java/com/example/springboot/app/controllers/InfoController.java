28690d349be7c64f483103825d65774004a1967d
USERSAD\jliriovi <jliriovi@everis.com>
2019-02-23 12:50:47 +0000

    Passing attributes from Controller to a View

--- Java/spring-boot-thymeleaf/src/main/java/com/example/springboot/app/controllers/InfoController.java
+++ Java/spring-boot-thymeleaf/src/main/java/com/example/springboot/app/controllers/InfoController.java
@@ -1,5 +1,6 @@
 package com.example.springboot.app.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,9 +8,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class InfoController {
 
+  @Value("${application.controllers.title}")
+  private String title;
+  
   @GetMapping("/")
   public String index(Model model) {
-    model.addAttribute("title", "Hello Spring Boot Thymeleaf!");
+    model.addAttribute("title", title);
     return "index";
   }
 }
