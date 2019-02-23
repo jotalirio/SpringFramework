28342dba4f918d64f30724e1e44637f1ed6e636b
USERSAD\jliriovi <jliriovi@everis.com>
2019-02-23 16:22:19 +0000

    Entity (Domain Class) 'client' created

---
+++ Java/spring-boot-data-jpa/src/main/java/com/example/springboot/app/models/entity/Client.java
@@ -0,0 +1,64 @@
+package com.example.springboot.app.models.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "clients")
+public class Client implements Serializable {
+
+  private static final long serialVersionUID = 1L;
+  
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  public Long id;
+  
+  public String name;
+  public String surname;
+  public String email;
+  
+  @Column(name = "creation_date")
+  @Temporal(TemporalType.DATE)
+  public Date creationDate;
+  
+  public Long getId() {
+    return id;
+  }
+  public void setId(Long id) {
+    this.id = id;
+  }
+  public String getName() {
+    return name;
+  }
+  public void setName(String name) {
+    this.name = name;
+  }
+  public String getSurname() {
+    return surname;
+  }
+  public void setSurname(String surname) {
+    this.surname = surname;
+  }
+  public String getEmail() {
+    return email;
+  }
+  public void setEmail(String email) {
+    this.email = email;
+  }
+  public Date getCreationDate() {
+    return creationDate;
+  }
+  public void setCreationDate(Date creationDate) {
+    this.creationDate = creationDate;
+  }
+  
+}
