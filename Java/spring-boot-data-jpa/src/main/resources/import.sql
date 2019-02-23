1704dc1bf5a6843bc6d0c7e23a9f1fd9d60ff1c7
USERSAD\jliriovi <jliriovi@everis.com>
2019-02-23 19:12:21 +0000

    Import.sql file with test data in order to test the application. Due to there is not a JPA provider for instance Hibernate configured. By default H2 database (in memory database) is used by JPA.

---
+++ Java/spring-boot-data-jpa/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+/* Populate tables: This file will be searched by JPA to populate the tables */
+INSERT INTO clients (id, name, surname, email, creation_date) VALUES(1, 'Jane', 'Doe', 'jane.doe@gmail.com', '2019-02-23');
+INSERT INTO clients (id, name, surname, email, creation_date) VALUES(2, 'John', 'Doe', 'john.doe@gmail.com', '2019-02-23');
