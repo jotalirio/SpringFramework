1d87f1bd9611301d5a2f652bd1de4a7accffae89
Jose Javier Lirio <jliriovi@everis.com>
2019-03-09 12:46:02 +0000

    - Refactoring the 'ClientControllerImpl.java' through relocating functions to a new file 'UploadFileServiceImpl.java' service.

--- Java/spring-boot-data-jpa/src/main/java/com/example/springboot/app/SpringBootDataJpaApplication.java
+++ Java/spring-boot-data-jpa/src/main/java/com/example/springboot/app/SpringBootDataJpaApplication.java
@@ -1,56 +1,29 @@
 package com.example.springboot.app;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.util.FileSystemUtils;
 
-import com.example.springboot.app.utils.Constants;
+import com.example.springboot.app.services.IUploadFileService;
 
 @SpringBootApplication
 public class SpringBootDataJpaApplication implements CommandLineRunner {
 
-  // We are going to use a directory (uploads/images) located inside the root project
-  private static final String UPLOADS_IMAGES_DIRECTORY_PROJECT_PATH = Constants.UPLOADS_DIRECTORY + "/" + Constants.UPLOADS_IMAGES_DIRECTORY;
+  @Autowired
+  IUploadFileService uploadFileService;
+  
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootDataJpaApplication.class, args);
+  }
 
-  private final Logger LOGGER = LoggerFactory.getLogger(SpringBootDataJpaApplication.class);
-
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootDataJpaApplication.class, args);
-	}
-
-	/**
-	 * When the project starts, the 'uploads/images' directory is automatically deleted and created in order to clear the images
-	 */
+  /**
+   * When the project starts, the 'uploads/images' directory is automatically deleted and created in order to clear the images
+   */
   @Override
   public void run(String... args) throws Exception {
-    this.deleteAll();
-    this.init();
+    this.uploadFileService.deleteAll();
+    this.uploadFileService.init();
   }
 
-  /**
-   * Delete the 'uploads/images' directory
-   */
-  private void deleteAll() {
-    FileSystemUtils.deleteRecursively(Paths.get(UPLOADS_IMAGES_DIRECTORY_PROJECT_PATH).toFile());
-  }
-  
-  /**
-   * Creates the 'uploads/images' directory
-   * @throws IOException
-   */
-  private void init() throws IOException {
-    try {
-      Files.createDirectories(Paths.get(UPLOADS_IMAGES_DIRECTORY_PROJECT_PATH));
-    } catch (IOException ex) {
-      LOGGER.error("ERROR: There was a problem creating the '" + UPLOADS_IMAGES_DIRECTORY_PROJECT_PATH + "' directory.");
-      throw ex;
-    }
-  }
 }
