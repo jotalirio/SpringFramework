11ba1ba1a33c2d11352d48b2276661ec5290e2da
USERSAD\jliriovi <jliriovi@everis.com>
2019-04-04 11:19:04 +0100

    - Adding the '@CrossOrigin' annotation.

--- Java/spring-boot-backend-apirest/src/main/java/com/example/springboot/backend/apirest/controllers/impl/ClientRestControllerImpl.java
+++ Java/spring-boot-backend-apirest/src/main/java/com/example/springboot/backend/apirest/controllers/impl/ClientRestControllerImpl.java
@@ -3,14 +3,18 @@ package com.example.springboot.backend.apirest.controllers.impl;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.springboot.backend.apirest.controllers.ClientRestController;
 import com.example.springboot.backend.apirest.models.entity.Client;
 import com.example.springboot.backend.apirest.services.ClientService;
 
+//@CrossOrigin(origins = {"http://localhost:4200"}, methods = {RequestMethod.GET})
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api")
 public class ClientRestControllerImpl implements ClientRestController {
