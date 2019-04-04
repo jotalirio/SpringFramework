bc7f3b333a365f116304e4606223de3f075d1b83
USERSAD\jliriovi <jliriovi@everis.com>
2019-04-04 10:28:19 +0100

    - Creating entity 'Client.java'

---
+++ Java/spring-boot-backend-apirest/src/main/java/com/example/springboot/backend/apirest/models/entity/Client.java
@@ -0,0 +1,86 @@
+package com.example.springboot.backend.apirest.models.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+@Table(name = "clients")
+public class Client implements Serializable {
+
+  private static final long serialVersionUID = 1L;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  
+  @NotEmpty
+  private String nombre;
+  
+  @NotEmpty
+  private String surname;
+  
+  @NotEmpty
+  @Email
+  private String email;
+  
+  @NotNull
+  @Column(name = "creation_date")
+  @Temporal(TemporalType.DATE) // Indicate we transform the Java Date to sql Date
+  @DateTimeFormat(pattern = "dd-MM-yyyy")
+  private Date creationDate;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getNombre() {
+    return nombre;
+  }
+
+  public void setNombre(String nombre) {
+    this.nombre = nombre;
+  }
+
+  public String getSurname() {
+    return surname;
+  }
+
+  public void setSurname(String surname) {
+    this.surname = surname;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public Date getCreationDate() {
+    return creationDate;
+  }
+
+  public void setCreationDate(Date creationDate) {
+    this.creationDate = creationDate;
+  }
+
+}
