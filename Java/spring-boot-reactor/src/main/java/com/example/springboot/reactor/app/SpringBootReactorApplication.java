518cb924ce9569128fab48754610c8192cc83018
USERSAD\jliriovi <jliriovi@everis.com>
2019-04-05 10:42:39 +0100

    - Implementing the 'CommandLineRunner' interface in 'SpringBootReactorApplication.java' class: With this we are changing this application as Cosole Application.

--- Java/spring-boot-reactor/src/main/java/com/example/springboot/reactor/app/SpringBootReactorApplication.java
+++ Java/spring-boot-reactor/src/main/java/com/example/springboot/reactor/app/SpringBootReactorApplication.java
@@ -1,13 +1,22 @@
 package com.example.springboot.reactor.app;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// To transform this SpringBoot app as a Console application we have to implement the 'CommandLineRunner' interface
 @SpringBootApplication
-public class SpringBootReactorApplication {
+public class SpringBootReactorApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootReactorApplication.class, args);
 	}
+
+	// This method is coming from the 'CommandLineRunner' interface
+  @Override
+  public void run(String... args) throws Exception {
+    
+    
+  }
 
 }
