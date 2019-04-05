d09721c333228f3e4101e71d92be1c9173844919
USERSAD\jliriovi <jliriovi@everis.com>
2019-04-05 15:04:00 +0100

    - Configuring the mongodb URL and changing the default MongoDB database used by default: from 'test' (the default one) to another. - Adding the 'creationDate' field to each Product when they are inserted into the MongoDB.

--- Java/spring-boot-webflux/src/main/java/com/example/springboot/webflux/app/SpringBootWebfluxApplication.java
+++ Java/spring-boot-webflux/src/main/java/com/example/springboot/webflux/app/SpringBootWebfluxApplication.java
@@ -1,11 +1,14 @@
 package com.example.springboot.webflux.app;
 
+import java.util.Date;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 
 import com.example.springboot.webflux.app.dao.ProductDao;
 import com.example.springboot.webflux.app.models.documents.Product;
@@ -20,6 +23,9 @@ public class SpringBootWebfluxApplication implements CommandLineRunner {
   @Autowired
   private ProductDao productDao;
   
+  @Autowired
+  private ReactiveMongoTemplate reactiveMongoTemplate;
+  
   
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootWebfluxApplication.class, args);
@@ -28,6 +34,12 @@ public class SpringBootWebfluxApplication implements CommandLineRunner {
 	// Implementing this method from CommandLineRunner we are going to create our mocked data in MongoDB
   @Override
   public void run(String... args) throws Exception {
+    
+    // Making create-drop each time we are running up the app. If we do not subscribe to the Mono<void> returned by the 'dropCollection()' method
+    // The method does not do nothing
+    this.reactiveMongoTemplate.dropCollection("products").subscribe();
+    
+    // Creating the Flux
     Flux.just(new Product("TV Panasonic", 456.89), 
               new Product("Sony Camara HD Digital", 177.89),
               new Product("Apple iPod", 46.89),
@@ -39,7 +51,11 @@ public class SpringBootWebfluxApplication implements CommandLineRunner {
               new Product("TV Sony Bravia OLED 4k Ultra HD", 2255.89))
         // 'flatMap' operator converts a Flux<Object> or Mono<Object> in a Object. We need it because for instance, 
         // the ProductDao.save() method is returning a Mono<Product> instead of Product object
-        .flatMap(product -> this.productDao.save(product))
+        .flatMap(product -> { 
+          product.setCreationDate(new Date());
+          return this.productDao.save(product); 
+        })
+        // We need to subscribe to the Flux if not there is none executed actions: inserting the products in the MongoDB 'products' collection
         .subscribe(product -> LOGGER.info("Insert: " + product.getId() + " " + product.getName()));
     
     
