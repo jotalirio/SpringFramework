25b89217a587326b2c9a15c6500a68fb40fbba4e
USERSAD\jliriovi <jliriovi@everis.com>
2019-04-05 15:31:01 +0100

    - Applying the 'map' operator and adding another suscriptor

--- Java/spring-boot-webflux/src/main/java/com/example/springboot/webflux/app/controllers/impl/ProductControllerImpl.java
+++ Java/spring-boot-webflux/src/main/java/com/example/springboot/webflux/app/controllers/impl/ProductControllerImpl.java
@@ -1,5 +1,7 @@
 package com.example.springboot.webflux.app.controllers.impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,14 +16,24 @@ import reactor.core.publisher.Flux;
 @Controller
 public class ProductControllerImpl implements ProductController {
 
+  private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+      
   @Autowired
   private ProductDao productDao;
   
   @GetMapping({"/", "/list"})
   public String list(Model model) {
-    Flux<Product> products = this.productDao.findAll();
+    Flux<Product> products = this.productDao.findAll().map(product -> {
+      product.setName(product.getName().toUpperCase());
+      return product;
+    });
+    
+    // Adding another observator or subscriptor to the Flux<Product> returned by the '.map()' operator. Remember that another subscriptor is the View
+    products.subscribe(product -> LOGGER.info("Product: " + product.getName()));
+    
+    // The View subscribe automatically to the Flux<Product> when we pass the flux to the View so the Thymeleaf template is the observator.
+    model.addAttribute("products", products); 
     model.addAttribute("title", "Products list");
-    model.addAttribute("products", products); // The View subscribe automatically to the Flux<Product> when we pass the flux to the View so the Thymeleaf template is the observator.
     return "list";
   }
 }
