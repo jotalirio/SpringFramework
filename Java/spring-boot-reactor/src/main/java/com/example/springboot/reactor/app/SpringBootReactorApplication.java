9bdb665d8b5520915ef5d6a13de3c9ed1dd9ab52
USERSAD\jliriovi <jliriovi@everis.com>
2019-04-05 10:59:57 +0100

    - Creating our first observable (Publisher) and subscribing to it.

--- Java/spring-boot-reactor/src/main/java/com/example/springboot/reactor/app/SpringBootReactorApplication.java
+++ Java/spring-boot-reactor/src/main/java/com/example/springboot/reactor/app/SpringBootReactorApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import reactor.core.publisher.Flux;
+
 // To transform this SpringBoot app as a Console application we have to implement the 'CommandLineRunner' interface
 @SpringBootApplication
 public class SpringBootReactorApplication implements CommandLineRunner {
@@ -16,6 +18,18 @@ public class SpringBootReactorApplication implements CommandLineRunner {
   @Override
   public void run(String... args) throws Exception {
     
+    // Creating our first observable flow. Flux is a 'Publisher', an observable and we can subscribe to him
+//    Flux<String> names = Flux.just("Jose", "Andres", "Juan", "Pedro")
+//                             .doOnNext(element -> { System.out.println(element); });// We are going to do some actions each time that we receive an element: Jose, Andres, Juan...
+    
+    
+    // Another equivalent way using Java 8
+    Flux<String> names = Flux.just("Jose", "Andres", "Juan", "Pedro")
+                             .doOnNext(System.out::println); // We transforming this lambda expression into a method reference. In this case System:out::println
+                                                             // That indicates that each 'name' have to be passed automatically to the 'println()' method
+    
+    // We have to subscribe to this observable (Publisher)
+    names.subscribe();
     
   }
 
