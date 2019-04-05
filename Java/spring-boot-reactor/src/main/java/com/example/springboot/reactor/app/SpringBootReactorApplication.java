1a2a6cf9c8ac66e16b11d9d6914ab3eef483c118
USERSAD\jliriovi <jliriovi@everis.com>
2019-04-05 11:19:16 +0100

    - Managing errors triggered from the observable (Publisher) to the observator that is subscribe to this observable reactive stream

--- Java/spring-boot-reactor/src/main/java/com/example/springboot/reactor/app/SpringBootReactorApplication.java
+++ Java/spring-boot-reactor/src/main/java/com/example/springboot/reactor/app/SpringBootReactorApplication.java
@@ -1,5 +1,7 @@
 package com.example.springboot.reactor.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,6 +12,8 @@ import reactor.core.publisher.Flux;
 @SpringBootApplication
 public class SpringBootReactorApplication implements CommandLineRunner {
 
+  private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+      
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootReactorApplication.class, args);
 	}
@@ -24,13 +28,36 @@ public class SpringBootReactorApplication implements CommandLineRunner {
     
     
     // Another equivalent way using Java 8
-    Flux<String> names = Flux.just("Jose", "Andres", "Juan", "Pedro")
-                             .doOnNext(System.out::println); // We transforming this lambda expression into a method reference. In this case System:out::println
+//    Flux<String> names = Flux.just("Jose", "Andres", "Juan", "Pedro")
+//                             .doOnNext(System.out::println); // We transforming this lambda expression into a method reference. In this case System:out::println
                                                              // That indicates that each 'name' have to be passed automatically to the 'println()' method
     
     // We have to subscribe to this observable (Publisher)
-    names.subscribe();
+//    names.subscribe(LOGGER::info); // We are going to do some actions each time that we receive an element: Jose, Andres, Juan...
+   
     
+    
+    // Managing errors triggered from the observable (Publisher) to the observator that is subscribe to this observable reactive stream
+    Flux<String> names = Flux.just("Jose", "Andres", "", "Juan", "Pedro")
+                             .doOnNext(e -> { 
+                                 if (e.isEmpty()) { // We are going to throw this exception when an element will be empty
+                                   throw new RuntimeException("Name must be not empty.");
+                                 }
+                                 System.out.println(e); 
+                             });
+
+    // We manage the subscribe method like in Angular 7
+    names.subscribe(
+      e -> {
+        LOGGER.info(e);
+      },
+      error -> {
+        // The error will be done when the observable (Publisher) is throwing the Exception. At this moment the stream is disrupted (interrumpido)
+        // out application will be still running as usual. Only the stream will be stopped
+        LOGGER.error(error.getMessage());
+      }
+    ); 
+     
   }
 
 }
